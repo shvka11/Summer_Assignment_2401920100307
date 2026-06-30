741677c4e910e5f218eb6d52cf45650ff68a7133
shvka11 <shivika330@gmail.com>
2026-06-30 08:04:24 +0530

    Update Day_1.java

--- Week_3/Day_1.java
+++ Week_3/Day_1.java
@@ -1,4 +1,48 @@
 public class Day_1 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+   //Question 1
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+   // Question 2
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next; 
+        }
+        return prev;
+    }
+   //Question 3
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return head = slow;
+    }
    
   
 }
