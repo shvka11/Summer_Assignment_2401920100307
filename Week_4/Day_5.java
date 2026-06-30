d54057bd2af28da3c8cb8bb54a6a41aa56b32c41
shvka11 <shivika330@gmail.com>
2026-07-01 00:04:06 +0530

    Update Day_5.java

--- Week_4/Day_5.java
+++ Week_4/Day_5.java
@@ -1,2 +1,24 @@
-public class Day_5 {
+import javapublic class Day_5 {
+  class Solution {
+    public boolean isSymmetric(TreeNode root) {
+       if(root == null)
+        return true;
+
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null)
+        return true;
+
+        if(left == null || right == null)
+        return false;
+
+        if(left.val != right.val)
+        return false;
+
+        return isMirror(left.left, right.right)
+        && isMirror(left.right, right.left);
+ 
+    }
+}
 }
