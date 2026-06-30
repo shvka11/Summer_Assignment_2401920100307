deec232e1ef818237891a475813e1e1e28650ae0
shvka11 <shivika330@gmail.com>
2026-07-01 00:07:33 +0530

    Update Day_5.java

--- Week_4/Day_5.java
+++ Week_4/Day_5.java
@@ -1,4 +1,7 @@
-import javapublic class Day_5 {
+
+ import java.util.*; 
+public class Day_5 {
+  // Question 1
   class Solution {
     public boolean isSymmetric(TreeNode root) {
        if(root == null)
@@ -21,4 +24,41 @@ import javapublic class Day_5 {
  
     }
 }
+  //Question 2
+  class Solution {
+     HashMap<Integer, Integer> map = new HashMap<>();
+    int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+
+        }
+
+        return helper(preorder,0,inorder.length - 1);
+
+    }
+
+    private TreeNode helper(int[] preorder, int left, int right){
+
+        if(left > right)
+        return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int mid = map.get(rootVal);
+
+        root.left = helper(preorder, left, mid - 1);
+        root.right = helper(preorder, mid + 1, right);
+
+        return root;
+    
+
+
+        
+    }
+}  
+    
+
 }
